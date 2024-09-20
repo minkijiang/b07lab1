4a7df13e1b4eea6a2efde26a07c677c0ccde3493
minkijiang <jiangminki0@gmail.com>
2024-09-20 13:48:27 -0400

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,44 @@
+import java.lang.Math;
+
+public class Polynomial {
+	double[] polynomialCoefficients;
+
+	public Polynomial() {
+		this.polynomialCoefficients = new double[1];
+		this.polynomialCoefficients[0] = 0;
+
+	}
+
+	public Polynomial(double[] polynomialCoefficients) {
+		this.polynomialCoefficients = polynomialCoefficients;
+	}
+
+	public Polynomial add(Polynomial polynomial) {
+		Polynomial polynomialSum;
+		double[] polynomialToAdd;
+
+		polynomialSum = new Polynomial(this.polynomialCoefficients);
+		polynomialToAdd =  polynomial.polynomialCoefficients;
+		if (polynomial.polynomialCoefficients.length > this.polynomialCoefficients.length) {
+			polynomialSum = new Polynomial(polynomial.polynomialCoefficients);
+			polynomialToAdd =  this.polynomialCoefficients;
+		}
+
+		for (int i = 0; i < polynomialToAdd.length; i++) {
+			polynomialSum.polynomialCoefficients[i] += polynomialToAdd[i];
+		}
+		return polynomialSum;
+	}
+
+	public double evaluate(double x) {
+		double sum = 0;
+		for (int i = 0; i < this.polynomialCoefficients.length; i++) {
+			sum += this.polynomialCoefficients[i] * (Math.pow(x,i));
+		}
+		return sum;
+	}
+
+	public boolean hasRoot(double x) {
+		return evaluate(x)==0;
+	}
+}
